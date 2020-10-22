f01cc42aad538ca818493f5896c621b9cbc6b266
Gutemberg Praia <gpa.hef3@uea.edu.br>
2020-10-22 09:01:05 -0400

    Exercicos adicionais - 4

---
+++ Exercicios adicionais - 4/fizzbuzz_funcao.py
@@ -0,0 +1,45 @@
+# Exercício 1 - FizzBuzz
+# Escreva a função fizzbuzz que recebe como parâmetro um número inteiro e devolve
+# 
+# 'Fizz' se o número for divisível por 3 e não for divisível por 5;
+# 
+# 'Buzz' se o número for divisível por 5 e não for divisível por 3;
+# 
+# 'FizzBuzz' se o número for divisível por 3 e por 5;
+# 
+# Caso o número não seja divisível 3 e também não seja divisível por 5, ela deve devolver o número recebido como parâmetro.
+# 
+# Exemplos de execução no Python Shell
+# >>>fizzbuzz(3)
+# "Fizz"
+# >>>fizzbuzz(5)
+# "Buzz"
+# >>>fizzbuzz(15)
+# "FizzBuzz"
+# >>>fizzbuzz(4)
+# 4
+#
+
+def fizzbuzz(num:int):
+    numDiv3 = False
+    numDiv5 = False
+
+    if(num%3==0):
+        numDiv3 = True
+
+    if (num%5==0):
+        numDiv5 = True
+
+    if(numDiv3 and not numDiv5):
+        return "Fizz"
+    elif(not numDiv3 and numDiv5):
+        return "Buzz"
+    elif(numDiv3 and numDiv5):
+        return "FizzBuzz"
+    elif not(numDiv3 and numDiv5):
+        return num
+
+print(fizzbuzz(3))
+print(fizzbuzz(5))
+print(fizzbuzz(15))
+print(fizzbuzz(4))
